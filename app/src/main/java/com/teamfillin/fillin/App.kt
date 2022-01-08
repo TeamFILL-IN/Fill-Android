896632eac10272951ee092a9de47dbacad7cb848
Hyun Woo Lee <l2hyunwoo@gmail.com>
2022-01-08 16:34:22 +0900

    [feature/#5] Flipper Config

--- app/src/main/java/com/teamfillin/fillin/App.kt
+++ app/src/main/java/com/teamfillin/fillin/App.kt
@@ -4,4 +4,9 @@ import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class App : Application()
+class App : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        FlipperInitializer.init(this)
+    }
+}
