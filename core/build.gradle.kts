0dab0745d5584594c3ff61632e033d6ab03f5bd8
Hyun Woo Lee <l2hyunwoo@gmail.com>
2022-01-09 17:04:06 +0900

    [feature/#14] Core module config

---
+++ core/build.gradle.kts
@@ -0,0 +1,62 @@
+plugins {
+    id("com.android.library")
+    kotlin("android")
+    kotlin("kapt")
+}
+
+android {
+    compileSdk = 31
+
+    defaultConfig {
+        minSdk = 26
+        targetSdk = 30
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = Versions.javaVersion
+        targetCompatibility = Versions.javaVersion
+    }
+
+    kotlinOptions {
+        jvmTarget = Versions.jvmVersion
+    }
+
+    buildFeatures {
+        dataBinding = true
+        viewBinding = true
+    }
+}
+
+dependencies {
+    // Kotlin
+    implementation(KotlinDependencies.kotlin)
+
+    // AndroidX
+    implementation(AndroidXDependencies.coreKtx)
+    implementation(AndroidXDependencies.appCompat)
+    implementation(AndroidXDependencies.constraintLayout)
+    implementation(AndroidXDependencies.coroutines)
+    implementation(AndroidXDependencies.fragment)
+
+    // Third-Party
+    implementation(ThirdPartyDependencies.glide)
+    kapt(KaptDependencies.glide)
+    implementation(ThirdPartyDependencies.gson)
+
+    // Test Dependency
+    testImplementation(TestDependencies.jUnit)
+    androidTestImplementation(TestDependencies.androidTest)
+    androidTestImplementation(TestDependencies.espresso)
+}
