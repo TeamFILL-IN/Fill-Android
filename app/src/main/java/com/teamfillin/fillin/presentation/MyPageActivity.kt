c6dccb9c6085dbd6f4e8c7763f8ec4e75be35263
Hyunji <24hyunji@naver.com>
2022-01-12 00:01:34 +0900

    [feature/#10] BindingActivity 적용 시도

--- app/src/main/java/com/teamfillin/fillin/presentation/MyPageActivity.kt
+++ app/src/main/java/com/teamfillin/fillin/presentation/MyPageActivity.kt
@@ -1,23 +1,20 @@
 package com.teamfillin.fillin.presentation
 
-import androidx.appcompat.app.AppCompatActivity
+
 import android.os.Bundle
 import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.LinearLayoutManager
 import com.teamfillin.fillin.MyPagePhotoRecyclerViewAdapter
 import com.teamfillin.fillin.Photos
 import com.teamfillin.fillin.R
+import com.teamfillin.fillin.core.base.BindingActivity
 import com.teamfillin.fillin.databinding.ActivityMyPageBinding
 
-class MyPageActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMyPageBinding
+class MyPageActivity : BindingActivity<ActivityMyPageBinding>(R.layout.activity_my_page) {
     private lateinit var adapter: MyPagePhotoRecyclerViewAdapter
     val photoDatas=mutableListOf<Photos>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding= ActivityMyPageBinding.inflate(layoutInflater)
-        setContentView(binding.root)
 
         initializelist()
         initPhotoRecyclerView()
