ba7edc73dd2bce08be1e42b33da414607bb5fa15
kimsubeen <papajj06@naver.com>
2022-01-12 02:21:20 +0900

    [feature/#12] popup 기능 구현

--- app/src/main/java/com/teamfillin/fillin/HomeActivity.kt
+++ app/src/main/java/com/teamfillin/fillin/HomeActivity.kt
@@ -2,25 +2,29 @@ package com.teamfillin.fillin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil.setContentView
 import com.teamfillin.fillin.databinding.ActivityHomeBinding
 import kotlinx.coroutines.newFixedThreadPoolContext
 
-class HomeActivity : AppCompatActivity(){
-    private lateinit var binding :ActivityHomeBinding
-    private lateinit var newPhotosAdapter : NewPhotosAdapter
+class HomeActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityHomeBinding
+    private lateinit var newPhotosAdapter: NewPhotosAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding= ActivityHomeBinding.inflate(layoutInflater)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
         initAdapter()
+        popup()
         setContentView(binding.root)
     }
 
-    private fun initAdapter(){
+
+    private fun initAdapter() {
         newPhotosAdapter = NewPhotosAdapter()
 
-        binding.rcvNewPhotos.adapter= newPhotosAdapter
+        binding.rcvNewPhotos.adapter = newPhotosAdapter
 
         val exphoto = R.drawable.and_photo_rectangle
 
@@ -37,5 +41,18 @@ class HomeActivity : AppCompatActivity(){
             )
         )
         newPhotosAdapter.notifyDataSetChanged()
+    }
+
+    fun popup() {
+        binding.apply{
+            btnClose.setOnClickListener {
+                if (clPopup.getVisibility() == View.VISIBLE)
+                    clPopup.setVisibility(View.GONE);
+
+            }
+            tvNotice.setOnClickListener{
+                Toast.makeText(this@HomeActivity, "현상소 제보 Page이동", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
