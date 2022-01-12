f9f6b51aef9b5a9304846090df6aae3eb1837cfd
Hyunji <24hyunji@naver.com>
2022-01-12 23:42:28 +0900

    [feature/#19] xml뷰 구현

---
+++ app/src/main/java/com/teamfillin/fillin/presentation/AddPhotoActivity.kt
@@ -0,0 +1,13 @@
+package com.teamfillin.fillin.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.teamfillin.fillin.R
+import com.teamfillin.fillin.core.base.BindingActivity
+import com.teamfillin.fillin.databinding.ActivityAddPhotoBinding
+
+class AddPhotoActivity : BindingActivity<ActivityAddPhotoBinding>(R.layout.activiy_add_photo) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
