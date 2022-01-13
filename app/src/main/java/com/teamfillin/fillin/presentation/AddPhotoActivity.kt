9b9ad55f5c26f1831c08ccf4d047af0c480226a5
Hyunji <24hyunji@naver.com>
2022-01-14 03:04:46 +0900

    [feature/#19] 커스텀 다이얼로그 뷰 추가

--- app/src/main/java/com/teamfillin/fillin/presentation/AddPhotoActivity.kt
+++ app/src/main/java/com/teamfillin/fillin/presentation/AddPhotoActivity.kt
@@ -1,12 +1,11 @@
 package com.teamfillin.fillin.presentation
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.teamfillin.fillin.R
 import com.teamfillin.fillin.core.base.BindingActivity
 import com.teamfillin.fillin.databinding.ActivityAddPhotoBinding
 
-class AddPhotoActivity : BindingActivity<ActivityAddPhotoBinding>(R.layout.activiy_add_photo) {
+class AddPhotoActivity : BindingActivity<ActivityAddPhotoBinding>(R.layout.activity_add_photo) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
