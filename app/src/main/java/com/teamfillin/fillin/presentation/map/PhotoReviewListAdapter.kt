e2c0ade4cd3171dd968a2f0bde6bf56fdb909d83
Leekangmin <kkk5474096@naver.com>
2022-01-16 23:26:44 +0900

    [feature/#25] 포토리뷰 RecyclerView Adapter

---
+++ app/src/main/java/com/teamfillin/fillin/presentation/map/PhotoReviewListAdapter.kt
@@ -0,0 +1,39 @@
+package com.teamfillin.fillin.presentation.map
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.teamfillin.fillin.data.ResponseLocationInfo
+import com.teamfillin.fillin.data.ResponsePhotoReviewInfo
+import com.teamfillin.fillin.databinding.ItemPhotoReviewBinding
+
+class PhotoReviewListAdapter :
+    RecyclerView.Adapter<PhotoReviewListAdapter.PhotoReviewListViewHolder>() {
+    private val photoList = mutableListOf<ResponsePhotoReviewInfo>()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PhotoReviewListViewHolder {
+        val binding =
+            ItemPhotoReviewBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return PhotoReviewListViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: PhotoReviewListViewHolder, position: Int) {
+        holder.onBind(photoList[position])
+    }
+
+    override fun getItemCount(): Int = photoList.size
+
+    fun setItem(newItems: List<ResponsePhotoReviewInfo>) {
+        photoList.clear()
+        photoList.addAll(newItems)
+        notifyDataSetChanged()
+    }
+
+    class PhotoReviewListViewHolder(
+        private val binding: ItemPhotoReviewBinding
+    ) : RecyclerView.ViewHolder(binding.root) {
+        fun onBind(photoReview: ResponsePhotoReviewInfo) {
+            binding.ivPhoto.setImageResource(photoReview.photo)
+        }
+    }
+}
