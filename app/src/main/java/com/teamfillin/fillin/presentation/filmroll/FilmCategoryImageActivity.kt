9e985fb9a3bc8b8c3d554b1790f8b2e518e4565f
Leekangmin <kkk5474096@naver.com>
2022-01-18 00:55:48 +0900

    [feature/#27] 필름 카테고리 이미지 액티비티 생성

---
+++ app/src/main/java/com/teamfillin/fillin/presentation/filmroll/FilmCategoryImageActivity.kt
@@ -0,0 +1,14 @@
+package com.teamfillin.fillin.presentation.filmroll
+
+import android.os.Bundle
+import com.teamfillin.fillin.R
+import com.teamfillin.fillin.core.base.BindingActivity
+import com.teamfillin.fillin.databinding.ActivityCategoryImageBinding
+import com.teamfillin.fillin.databinding.ActivityFilmRollCategoryBinding
+
+class FilmCategoryImageActivity: BindingActivity<ActivityCategoryImageBinding>(R.layout.activity_category_image) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+}
