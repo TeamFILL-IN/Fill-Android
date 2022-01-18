a764fee9904f23106c42c664352e3f0c907a24ad
Leekangmin <kkk5474096@naver.com>
2022-01-18 17:22:31 +0900

    [feature/#32] StudioService interface

---
+++ app/src/main/java/com/teamfillin/fillin/data/service/StudioService.kt
@@ -0,0 +1,10 @@
+package com.teamfillin.fillin.data.service
+
+import com.teamfillin.fillin.data.response.ResponseStudioInfo
+import retrofit2.Call
+import retrofit2.http.GET
+
+interface StudioService {
+    @GET("studio")
+    fun getWholeStudio(): Call<ResponseStudioInfo>
+}
