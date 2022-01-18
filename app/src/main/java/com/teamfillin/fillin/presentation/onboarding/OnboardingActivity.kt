63f56b9fff591a107fd053b74ffc28ce70067c4f
Hyun Woo Lee <l2hyunwoo@gmail.com>
2022-01-19 01:24:02 +0900

    [feature/#41] 온보딩 화면 추가 밒 관련 리소스 추가

---
+++ app/src/main/java/com/teamfillin/fillin/presentation/onboarding/OnboardingActivity.kt
@@ -0,0 +1,13 @@
+package com.teamfillin.fillin.presentation.onboarding
+
+import android.os.Bundle
+import com.teamfillin.fillin.R
+import com.teamfillin.fillin.core.base.BindingActivity
+import com.teamfillin.fillin.databinding.ActivityOnboardingBinding
+
+class OnboardingActivity :
+    BindingActivity<ActivityOnboardingBinding>(R.layout.activity_onboarding) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
