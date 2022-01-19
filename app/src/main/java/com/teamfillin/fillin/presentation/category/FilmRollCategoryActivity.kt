cff67dd6b96ab5eb128d1fa5f73fa705d682b5e8
Hyun Woo Lee <l2hyunwoo@gmail.com>
2022-01-19 10:07:59 +0900

    [feature/#41] 불필요한 의존성 삭제

--- app/src/main/java/com/teamfillin/fillin/presentation/category/FilmRollCategoryActivity.kt
+++ app/src/main/java/com/teamfillin/fillin/presentation/category/FilmRollCategoryActivity.kt
@@ -3,28 +3,14 @@ package com.teamfillin.fillin.presentation.category
 import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
-import android.view.MenuItem
-import android.view.View
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.view.get
-import androidx.core.view.isVisible
-import androidx.recyclerview.widget.ListAdapter
-import com.google.android.material.tabs.TabLayout
-import com.google.android.material.tabs.TabLayoutMediator
 import com.teamfillin.fillin.R
-import com.teamfillin.fillin.com.teamfillin.fillin.presentation.home.NEW_PHOTOS_TYPE
-import com.teamfillin.fillin.com.teamfillin.fillin.presentation.home.NEXT_BUTTON_TYPE
 import com.teamfillin.fillin.core.base.BindingActivity
-import com.teamfillin.fillin.core.context.toast
 import com.teamfillin.fillin.core.view.setOnSingleClickListener
 import com.teamfillin.fillin.data.CategoryInfo
-
 import com.teamfillin.fillin.databinding.ActivityFilmRollCategoryBinding
-import com.teamfillin.fillin.presentation.AddPhotoActivity
 import com.teamfillin.fillin.presentation.filmroll.FilmCategoryImageActivity
 import com.teamfillin.fillin.presentation.map.CustomDecoration
-import com.teamfillin.fillin.presentation.map.LocationListAdapter
-import com.teamfillin.fillin.presentation.map.MapSearchActivity
 
 class FilmRollCategoryActivity :
     BindingActivity<ActivityFilmRollCategoryBinding>(R.layout.activity_film_roll_category) {
@@ -44,7 +30,7 @@ class FilmRollCategoryActivity :
     }
 
     private fun click() {
-        filmCategoryAdapter.setItemClickListener { v, position ->
+        filmCategoryAdapter.setItemClickListener { _, position ->
             // ToDO 서버연결 시 버튼 클릭 시 데이터 가지고 FilmCategoryImage액티비티 이동
 
             binding.rvCategory[position].setOnSingleClickListener {
