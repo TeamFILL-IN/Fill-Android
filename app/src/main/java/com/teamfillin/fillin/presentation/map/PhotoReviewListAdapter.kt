de8aacc04e663322fe5c00d6bdb51dce9a9d2d77
Leekangmin <kkk5474096@naver.com>
2022-01-20 17:10:32 +0900

    [feature/#47] RecyclerViewAdapter -> ListAdapter 변경

--- app/src/main/java/com/teamfillin/fillin/presentation/map/PhotoReviewListAdapter.kt
+++ app/src/main/java/com/teamfillin/fillin/presentation/map/PhotoReviewListAdapter.kt
@@ -2,15 +2,17 @@ package com.teamfillin.fillin.presentation.map
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.teamfillin.fillin.data.response.ResponseStudioPhoto
 import com.teamfillin.fillin.databinding.ItemPhotoReviewBinding
-import timber.log.Timber
 
 class PhotoReviewListAdapter :
-    RecyclerView.Adapter<PhotoReviewListAdapter.PhotoReviewListViewHolder>() {
-    private val photoList = mutableListOf<ResponseStudioPhoto.StudioPhoto>()
+    ListAdapter<ResponseStudioPhoto.StudioPhoto, PhotoReviewListAdapter.PhotoReviewListViewHolder>(
+        DIFFUTIL
+    ) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PhotoReviewListViewHolder {
         val binding =
@@ -19,16 +21,7 @@ class PhotoReviewListAdapter :
     }
 
     override fun onBindViewHolder(holder: PhotoReviewListViewHolder, position: Int) {
-        Timber.d("Nunu ${photoList[position]}")
-        holder.onBind(photoList[position])
-    }
-
-    override fun getItemCount(): Int = photoList.size
-
-    fun setItem(newItems: List<ResponseStudioPhoto.StudioPhoto>) {
-        photoList.clear()
-        photoList.addAll(newItems)
-        notifyDataSetChanged()
+        holder.onBind(getItem(position))
     }
 
     class PhotoReviewListViewHolder(
@@ -36,6 +29,24 @@ class PhotoReviewListAdapter :
     ) : RecyclerView.ViewHolder(binding.root) {
         fun onBind(studioPhoto: ResponseStudioPhoto.StudioPhoto) {
             Glide.with(itemView.context).load(studioPhoto.imageUrl).into(binding.ivPhoto)
+        }
+    }
+
+    companion object {
+        val DIFFUTIL = object : DiffUtil.ItemCallback<ResponseStudioPhoto.StudioPhoto>() {
+            override fun areItemsTheSame(
+                oldItem: ResponseStudioPhoto.StudioPhoto,
+                newItem: ResponseStudioPhoto.StudioPhoto
+            ): Boolean {
+                return oldItem.photoId == newItem.photoId
+            }
+
+            override fun areContentsTheSame(
+                oldItem: ResponseStudioPhoto.StudioPhoto,
+                newItem: ResponseStudioPhoto.StudioPhoto
+            ): Boolean {
+                return oldItem == newItem
+            }
         }
     }
 }
