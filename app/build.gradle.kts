768685a740b909daffff9d68edfc1386732ca512
Hyun Woo Lee <l2hyunwoo@gmail.com>
2022-02-21 14:49:32 +0900

    [feature/#79] 앱 난독화 및 리소스 최적화 옵션 적용

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,7 +43,8 @@ android {
 
         getByName("release") {
             signingConfig = signingConfigs.getByName("release")
-            isMinifyEnabled = false
+            isMinifyEnabled = true
+            isShrinkResources = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
