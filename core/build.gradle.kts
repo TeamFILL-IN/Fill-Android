db17c7eadb8f7abc4d97fafd8a705e1eb7fe2d1b
Hyun Woo Lee <l2hyunwoo@gmail.com>
2022-02-21 16:49:03 +0900

    [feature/#79] 타 모듈의 proguard 설정 삭제

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -14,16 +14,6 @@ android {
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("consumer-rules.pro")
     }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
     compileOptions {
         sourceCompatibility = Versions.javaVersion
         targetCompatibility = Versions.javaVersion
