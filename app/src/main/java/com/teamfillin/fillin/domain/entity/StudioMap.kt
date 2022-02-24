affacb6103e1578f7f694b435dc5c1bc92b7fe2a
Leekangmin <kkk5474096@naver.com>
2022-02-24 19:02:57 +0900

    [feature/#77] domain StudioMap

---
+++ app/src/main/java/com/teamfillin/fillin/domain/entity/StudioMap.kt
@@ -0,0 +1,11 @@
+package com.teamfillin.fillin.domain.entity
+
+data class StudioMap (
+    val studios: List<StudioPosition>
+) {
+    data class StudioPosition(
+        val id: Int,
+        val lati: Double,
+        val long: Double
+    )
+}
