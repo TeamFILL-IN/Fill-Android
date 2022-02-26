15b1315184655493d4f0d3b198691fc77ca38fa6
Leekangmin <kkk5474096@naver.com>
2022-02-26 16:48:47 +0900

    [feature/#77] StudioMapRepositoryImpl 생성

--- app/src/main/java/com/teamfillin/fillin/data/repository/StudioMapRepositoryImpl.kt
+++ app/src/main/java/com/teamfillin/fillin/data/repository/StudioMapRepositoryImpl.kt
@@ -1,9 +1,11 @@
 package com.teamfillin.fillin.data.repository
 
 import com.teamfillin.fillin.data.service.StudioService
+import com.teamfillin.fillin.domain.entity.StudioDetail
 import com.teamfillin.fillin.domain.entity.StudioMap
 import com.teamfillin.fillin.domain.repository.StudioMapRepository
 import retrofit2.await
+import timber.log.Timber
 import javax.inject.Inject
 
 class StudioMapRepositoryImpl @Inject constructor(
@@ -17,5 +19,13 @@ class StudioMapRepositoryImpl @Inject constructor(
                 response.toStudioMap()
             }
         }, { return null })
+    }
+
+    override suspend fun studioDetail(position : Int): StudioDetail.Studio? {
+        runCatching {
+            service.getStudioDetail(position).await()
+        }.fold({
+            return it.data.studio.toStudioDetail()
+        }, {return null})
     }
 }
