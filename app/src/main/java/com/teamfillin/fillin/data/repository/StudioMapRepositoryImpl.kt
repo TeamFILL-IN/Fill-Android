fd71d2e450937da0ec8efa42094b3d2a6e8b3234
Leekangmin <kkk5474096@naver.com>
2022-02-27 13:42:15 +0900

    [feature/#77] StudioMapRepositoryImpl 구현

--- app/src/main/java/com/teamfillin/fillin/data/repository/StudioMapRepositoryImpl.kt
+++ app/src/main/java/com/teamfillin/fillin/data/repository/StudioMapRepositoryImpl.kt
@@ -2,6 +2,7 @@ package com.teamfillin.fillin.data.repository
 
 import com.teamfillin.fillin.data.service.StudioService
 import com.teamfillin.fillin.domain.entity.StudioDetail
+import com.teamfillin.fillin.domain.entity.StudioImage
 import com.teamfillin.fillin.domain.entity.StudioMap
 import com.teamfillin.fillin.domain.repository.StudioMapRepository
 import retrofit2.await
@@ -11,21 +12,23 @@ import javax.inject.Inject
 class StudioMapRepositoryImpl @Inject constructor(
     private val service: StudioService
 ) : StudioMapRepository {
-    override suspend fun studioLocation(): List<StudioMap.StudioPosition>? {
-        runCatching {
-            service.getWholeStudio().await()
-        }.fold({
-            return it.data.studios.map { response ->
+    override suspend fun studioLocation(): List<StudioMap.StudioPosition> {
+        // return service.getWholeStudio().await().data.studios.toEntity()
+        return service.getWholeStudio().await()
+            .data.studios.map { response ->
                 response.toStudioMap()
             }
-        }, { return null })
     }
 
-    override suspend fun studioDetail(position : Int): StudioDetail.Studio? {
-        runCatching {
-            service.getStudioDetail(position).await()
-        }.fold({
-            return it.data.studio.toStudioDetail()
-        }, {return null})
+    override suspend fun studioDetail(position: Int): StudioDetail {
+        return service.getStudioDetail(position).await()
+            .data.studio.toStudioDetail()
+    }
+
+    override suspend fun studioPhoto(position: Int): List<StudioImage> {
+        return service.getStudioPhoto(position).await()
+            .data.photos.map { response ->
+                response.toStudioImage()
+            }
     }
 }
