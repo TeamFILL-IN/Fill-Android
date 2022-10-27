e1059ad83914db9c6057c1d2a0fc040ddbdb1b82
Leekangmin <kkk5474096@naver.com>
2022-10-28 00:29:21 +0900

    [refactor/#87] 패키징

--- app/src/main/java/com/teamfillin/fillin/presentation/filmroll/FilmRollViewModel.kt
+++ app/src/main/java/com/teamfillin/fillin/presentation/filmroll/FilmRollViewModel.kt
@@ -6,7 +6,7 @@ import androidx.paging.Pager
 import androidx.paging.PagingConfig
 import androidx.paging.cachedIn
 import androidx.paging.map
-import com.teamfillin.fillin.data.remote.CategoryPhotosPagingSource
+import com.teamfillin.fillin.data.datasource.CategoryPhotosPagingSource
 import com.teamfillin.fillin.data.service.PagingService
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.map
