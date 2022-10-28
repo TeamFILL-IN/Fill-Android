3ff8729bb4ccd2a1e9144d7effc27e55d450a3a2
Leekangmin <kkk5474096@naver.com>
2022-10-28 18:51:20 +0900

    [feature/#90] MyPageActivity 로직 수정

--- app/src/main/java/com/teamfillin/fillin/presentation/my/MyPageActivity.kt
+++ app/src/main/java/com/teamfillin/fillin/presentation/my/MyPageActivity.kt
@@ -2,46 +2,45 @@ package com.teamfillin.fillin.presentation.my
 
 import android.content.ClipData
 import android.content.ClipboardManager
-import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import androidx.core.content.getSystemService
+import android.widget.ImageView
+import androidx.activity.viewModels
+import androidx.core.view.isVisible
+import androidx.lifecycle.flowWithLifecycle
 import androidx.lifecycle.lifecycleScope
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
-import com.google.android.gms.oss.licenses.OssLicensesMenuActivity
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.kakao.sdk.user.UserApiClient
 import com.teamfillin.fillin.BuildConfig
 import com.teamfillin.fillin.R
 import com.teamfillin.fillin.core.base.BindingActivity
+import com.teamfillin.fillin.core.context.toast
+import com.teamfillin.fillin.core.view.UiState
 import com.teamfillin.fillin.core.view.setOnSingleClickListener
-import com.teamfillin.fillin.data.service.MyPagePhotoService
 import com.teamfillin.fillin.data.service.UserService
 import com.teamfillin.fillin.databinding.ActivityMyPageBinding
 import com.teamfillin.fillin.presentation.dialog.PhotoDialogFragment
-import com.teamfillin.fillin.presentation.login.KakaoAuthService
 import com.teamfillin.fillin.presentation.map.SpaceDecoration
+import com.teamfillin.fillin.presentation.my.terms.TermsActivity
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.launch
-import retrofit2.await
-import timber.log.Timber
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 import javax.inject.Inject
 
 @AndroidEntryPoint
 class MyPageActivity : BindingActivity<ActivityMyPageBinding>(R.layout.activity_my_page) {
-    @Inject
-    lateinit var userService: UserService
+    private val viewModel by viewModels<MyPageViewModel>()
 
     @Inject
-    lateinit var myPhotoService: MyPagePhotoService
+    lateinit var userService: UserService
 
     @Inject
     lateinit var client: UserApiClient
 
     private val adapter = MyPagePhotoRecyclerViewAdapter {
         val dialog = PhotoDialogFragment()
-        Timber.d("mypagekangmin")
         val bundle = Bundle().apply { putString("photoUrl", it.imageUrl) }
         dialog.apply {
             arguments = bundle
@@ -51,9 +50,34 @@ class MyPageActivity : BindingActivity<ActivityMyPageBinding>(R.layout.activity_
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        initView()
+        initEvent()
+        initAdapter()
+        observe()
+
+    }
+
+    private fun initView() {
         binding.isDebug = BuildConfig.DEBUG
+        viewModel.getUser()
+        viewModel.getUserPhotos()
+    }
+
+    private fun initEvent() {
         binding.btnBackHome.setOnClickListener {
             finish()
+        }
+
+        binding.ivUp.setOnSingleClickListener {
+            binding.rvMyPage.isVisible = !binding.rvMyPage.isVisible
+            imageRotation(binding.ivUp, binding.rvMyPage.isVisible)
+        }
+
+        binding.tvTerms.setOnSingleClickListener {
+            Intent(this, TermsActivity::class.java).apply {
+                startActivity(this)
+            }
         }
         binding.btnSettingDebug.setOnSingleClickListener {
             client.loginWithKakaoAccount(this, callback = { token, _ ->
@@ -75,13 +99,6 @@ class MyPageActivity : BindingActivity<ActivityMyPageBinding>(R.layout.activity_
                     }.show()
             })
         }
-        binding.btnSetting.setOnClickListener {
-            startActivity(Intent(this, OssLicensesMenuActivity::class.java))
-        }
-
-        initAdapter()
-        showUserInfo()
-        showMyPhoto()
     }
 
     private fun initAdapter() {
@@ -90,28 +107,44 @@ class MyPageActivity : BindingActivity<ActivityMyPageBinding>(R.layout.activity_
         binding.rvMyPage.addItemDecoration(spaceDecoration)
     }
 
-    private fun showUserInfo() {
-        lifecycleScope.launch {
-            runCatching {
-                userService.getUserInfo().await()
-            }.onSuccess {
-                binding.tvNickname.text = it.data.user.nickname
-                Glide.with(this@MyPageActivity)
-                    .load(it.data.user.imageUrl)
-                    .diskCacheStrategy(DiskCacheStrategy.RESOURCE)
-                    .circleCrop()
-                    .into(binding.ivProfile)
-            }.onFailure(Timber::e)
-        }
+    private fun observe() {
+        viewModel.userInfo.flowWithLifecycle(lifecycle)
+            .onEach {
+                when (it) {
+                    is UiState.Success -> {
+                        binding.tvNickname.text = it.data.nickname
+                        Glide.with(this@MyPageActivity)
+                            .load(it.data.imageUrl)
+                            .diskCacheStrategy(DiskCacheStrategy.RESOURCE)
+                            .circleCrop()
+                            .into(binding.ivProfile)
+                    }
+                    is UiState.Failure -> {
+                        toast(it.msg)
+                    }
+                    else -> {}
+                }
+            }.launchIn(lifecycleScope)
+
+        viewModel.myPhotos.flowWithLifecycle(lifecycle)
+            .onEach {
+                when (it) {
+                    is UiState.Success -> {
+                        adapter.replaceList(it.data)
+                    }
+                    is UiState.Failure -> {
+                        toast(it.msg)
+                    }
+                    else -> {}
+                }
+            }.launchIn(lifecycleScope)
     }
 
-    private fun showMyPhoto() {
-        lifecycleScope.launch {
-            runCatching {
-                myPhotoService.getUserPhotos().await()
-            }.onSuccess {
-                adapter.replaceList(it.data.photos)
-            }.onFailure(Timber::e)
+    private fun imageRotation(image: ImageView, isExpand: Boolean) {
+        if (isExpand) {
+            image.animate().setDuration(200).rotation(0f)
+        } else {
+            image.animate().setDuration(200).rotation(180f)
         }
     }
 }
