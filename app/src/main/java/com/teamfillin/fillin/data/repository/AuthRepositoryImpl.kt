0af17d0d414d5b80c85a0c0a8436aae868550043
Leekangmin <kkk5474096@naver.com>
2022-10-28 18:50:00 +0900

    [feature/#90] AuthRepository & Impl

--- app/src/main/java/com/teamfillin/fillin/data/repository/AuthRepositoryImpl.kt
+++ app/src/main/java/com/teamfillin/fillin/data/repository/AuthRepositoryImpl.kt
@@ -1,9 +1,11 @@
 package com.teamfillin.fillin.data.repository
 
+import com.teamfillin.fillin.data.datasource.AuthDataSource
 import com.teamfillin.fillin.data.local.FillInDataStore
 import com.teamfillin.fillin.data.response.toEntity
 import com.teamfillin.fillin.data.service.AuthService
 import com.teamfillin.fillin.domain.entity.Auth
+import com.teamfillin.fillin.domain.entity.User
 import com.teamfillin.fillin.domain.repository.AuthRepository
 import retrofit2.await
 import timber.log.Timber
@@ -11,7 +13,8 @@ import javax.inject.Inject
 
 class AuthRepositoryImpl @Inject constructor(
     private val dataStore: FillInDataStore,
-    private val service: AuthService
+    private val service: AuthService,
+    private val dataSource: AuthDataSource
 ) : AuthRepository {
     override suspend fun login(token: String, id: String) {
         runCatching {
@@ -24,5 +27,9 @@ class AuthRepositoryImpl @Inject constructor(
             }
             if (auth is Auth.SignUp) dataStore.nickname = auth.nickName
         }, Timber::e)
+    }
+
+    override suspend fun getUser(): User {
+        return dataSource.getUser().toUser()
     }
 }
--- app/src/main/java/com/teamfillin/fillin/domain/repository/AuthRepository.kt
+++ app/src/main/java/com/teamfillin/fillin/domain/repository/AuthRepository.kt
@@ -1,5 +1,8 @@
 package com.teamfillin.fillin.domain.repository
 
+import com.teamfillin.fillin.domain.entity.User
+
 interface AuthRepository {
     suspend fun login(token: String, id: String)
+    suspend fun getUser(): User
 }
