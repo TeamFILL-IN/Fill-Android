2f4bb69a12d43a45493c47a3b12452893bf2bb9d
Leekangmin <kkk5474096@naver.com>
2023-01-04 18:13:30 +0900

    [feature/#92] AuthService 프로필 수정

--- app/src/main/java/com/teamfillin/fillin/data/service/AuthService.kt
+++ app/src/main/java/com/teamfillin/fillin/data/service/AuthService.kt
@@ -4,6 +4,9 @@ import com.teamfillin.fillin.core.converter.JsonConverter
 import com.teamfillin.fillin.data.response.BaseResponse
 import com.teamfillin.fillin.data.response.ResponseAuth
 import com.teamfillin.fillin.data.response.ResponseRefresh
+import com.teamfillin.fillin.data.response.ResponseUpdateUser
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
 import retrofit2.Call
 import retrofit2.http.*
 
@@ -22,4 +25,11 @@ interface AuthService {
         @Header("accesstoken") accessToken: String,
         @Header("refreshtoken") refreshToken: String
     ): Call<BaseResponse<ResponseRefresh>>
+
+    @Multipart
+    @PUT("user/edit")
+    suspend fun putUser(
+        @Part media: MultipartBody.Part?,
+        @PartMap body: HashMap<String, RequestBody>
+    ): BaseResponse<ResponseUpdateUser>
 }
